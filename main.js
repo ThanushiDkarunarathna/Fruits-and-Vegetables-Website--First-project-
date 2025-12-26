2d94251853d15eb3d43726a04302eabd53e5f274
Thanushi Dhananjalee <thanushidhananjalee21@gmail.com>
2025-12-26 13:57:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//Swiper
+document.addEventListener('DOMContentLoaded', function() {
+  var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        spaceBetween: 10,
+      },
+      480: {
+        spaceBetween: 0,
+      },
+    },
+  });
+});
+
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
